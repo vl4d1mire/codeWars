cb6df6a4cf19ee35b612ef7953eea893e7d3062a
vl4d1mire <egv_89@mail.ru>
2021-06-10 20:46:17 +0300

    1 file modified

--- src/6 kyu/Build Tower/index.js
+++ src/6 kyu/Build Tower/index.js
@@ -1,10 +1,10 @@
 function towerBuilder(nFloors) {
-    let space, stairs, resultArr;
+    let space, stars, resultArr;
     resultArr = [];
     for (let i = 1; i <= nFloors; i++) {
         space = ' '.repeat(nFloors - i);
         stairs = '*'.repeat(i * 2 - 1);
-        resultArr.push(`${space}${stairs}${space}`);
+        resultArr.push(`${space}${stars}${space}`);
     }
     return resultArr;
 }
